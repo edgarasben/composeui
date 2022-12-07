bbb17de0ed890674d83a5de123cb901ba7530f79
Edgaras Benediktavicius <725324+edgarasben@users.noreply.github.com>
2022-12-08 00:30:49 +0100

    remove dist

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Loader = exports.Button = void 0;
-var button_1 = require("./button");
-Object.defineProperty(exports, "Button", { enumerable: true, get: function () { return button_1.Button; } });
-var loader_1 = require("./loader");
-Object.defineProperty(exports, "Loader", { enumerable: true, get: function () { return loader_1.Loader; } });
